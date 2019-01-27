d9a1883c4e3f5b403451d43907b96dfc85a0af9b
San <sanpc@qq.com>
2019-01-27 16:48:23 +0800

    周一 添加商品详情功能

--- src/api/index.js
+++ src/api/index.js
@@ -76,3 +76,7 @@ export const reqRoles = () => ajax('/manage/role/list')
 
 //添加角色
 export const reqAddRole = (roleName) => ajax('/manage/role/add', {roleName}, 'POST')
+
+
+//更新角色（授权）
+export const reqAuthorizeRoles = (role) => ajax('/manage/role/update', role, 'POST')
